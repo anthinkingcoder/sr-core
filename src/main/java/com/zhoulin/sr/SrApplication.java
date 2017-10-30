bee9325db9580209b52d6484b5db6f47d068b5e9
周琳 <837769723@qq.com>
2017-10-30 10:50:03 +0800

    init project

---
+++ src/main/java/com/zhoulin/sr/SrApplication.java
@@ -0,0 +1,12 @@
+package com.zhoulin.sr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrApplication.class, args);
+	}
+}
