cb2da3c86fafb2a3bd0dc1f0680885e50643006a
周琳 <837769723@qq.com>
2017-11-02 11:42:40 +0800

    资源管理服务器

---
+++ sr-fs/src/main/java/com/sp/sr/fs/FsApplication.java
@@ -0,0 +1,12 @@
+package com.sp.sr.fs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsApplication.class, args);
+	}
+}
